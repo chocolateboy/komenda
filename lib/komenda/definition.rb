49e81e034defa8740c28641500c8151c9172506f
Reto Kaiser <reto@cargomedia.ch>
2015-11-01 19:37:22 +0100

    Add options.env for definition

--- lib/komenda/definition.rb
+++ lib/komenda/definition.rb
@@ -2,10 +2,18 @@ module Komenda
   class Definition
 
     attr_reader :cmd
+    attr_reader :env
 
     # @param [String] cmd
-    def initialize(cmd)
-      @cmd = cmd
+    # @param [Hash] options
+    def initialize(cmd, options = {})
+      defaults = {
+        :env => ENV.to_hash,
+      }
+      options = defaults.merge(options)
+
+      @cmd = String(cmd)
+      @env = Hash(options[:env])
     end
 
   end
--- spec/lib/komenda/definition_spec.rb
+++ spec/lib/komenda/definition_spec.rb
@@ -4,10 +4,25 @@ describe Komenda::Definition do
 
   describe '#initialize' do
     let(:cmd) { 'my command' }
-    let(:definition) { Komenda::Definition.new(cmd) }
 
-    it 'sets the cmd' do
-      expect(definition.cmd).to eq(cmd)
+    context 'when passing an ENV' do
+      let(:definition) { Komenda::Definition.new(cmd, {:env => {:foo => 'bar'}}) }
+
+      it 'sets the cmd' do
+        expect(definition.cmd).to eq(cmd)
+      end
+
+      it 'sets the environment' do
+        expect(definition.env).to eq({:foo => 'bar'})
+      end
+    end
+
+    context 'when not passing options' do
+      let(:definition) { Komenda::Definition.new(cmd) }
+
+      it 'uses the env from the current process' do
+        expect(definition.env).to eq(ENV.to_hash)
+      end
     end
   end
 
